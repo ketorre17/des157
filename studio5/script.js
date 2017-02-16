8e6a896848aaf7dc6ae16e33947989deba9acd94
ketorre17 <ketorres@ucdavis.edu>
2017-02-16 11:43:10 -0800

    02-16-2017
    studio 5 library exploration

---
+++ studio5/script.js
@@ -0,0 +1,48 @@
+// Katrina Torres / DES157 Winter2017 / 01-26-2017
+/* This is the script.js for "Meet me at the library" exercising. Learning utilize a library (jsPDF)*/
+
+// use 'console.log()' and' open console window for troubleshooting
+console.log('reading');
+
+//dom code
+document.addEventListener ("DOMContentLoaded", function(event){
+  console.log("DOM fully loaded and parsed");
+  document.formX.onsubmit = processForm;
+  function processForm() {
+    //start variables
+    var looking = document.formX.looking.value;
+    var descript = document.formX.descript.value;
+    var meet = document.formX.meet.value;
+
+    var doc = new jsPDF ();
+    /*doc.text('string',x,y)
+    doc=name of the pdf in js
+    .text=type of input
+    
+    */
+    doc.text('I AM THE PDF', 10, 10);
+    doc.text(looking, 50, 50)
+    doc.save('theAd.pdf');
+
+    /*form x submit test*/
+    var test01 = document.getElementById('test01');
+    var test02 = document.getElementById('test02');
+    var test03 = document.getElementById('test03');
+
+    test01.innerHTML="Looking for: " + looking;
+    test02.innerHTML="Must be: " +descript;
+    test03.innerHTML="if discovered, meet me at: " + meet;
+
+    return false;
+
+
+  }
+})
+
+
+
+/*
+useful links
+http://rawgit.com/MrRio/jsPDF/master/
+http://rawgit.com/MrRio/jsPDF/master/docs/
+*/
